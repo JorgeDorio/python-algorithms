f21c0ff099675b00c1870d247d4f4f70e684eb06
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-11-27 13:32:57 -0300

    4 - Anagramas (Algoritmo de ordenação)

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,15 @@
+def sorting(string):
+    letter = string.lower()
+    letter = letter.replace(" ", "")
+    letter = list(letter)
+    n = len(string)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if letter[j] > letter[j+1]:
+                letter[j], letter[j+1] = letter[j+1], letter[j]
+    return "".join(letter)
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = sorting(first_string)
+    second = sorting(second_string)
+    return (first, second, first == second)
