a03867342dd2f0a6bcc313fcef651e103c837808
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-11-27 12:33:03 -0300

    2 - Criptografia de inversões

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,6 @@
 def study_schedule(permanence_period, target_time):
     try:
         students = 0
-
         for start, end in permanence_period:
             if start <= target_time <= end:
                 students += 1
